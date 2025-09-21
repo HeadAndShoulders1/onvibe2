fa9dac585f264d373a3db9ca7397c41a58f4643e
tighthealer <125491963+HeadAndShoulders1@users.noreply.github.com>
2025-09-22 00:17:01 +0300

    Update route.ts565432

--- app/api/news/get/route.ts
+++ app/api/news/get/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from "@/lib/prisma";
 
+// Явно указываем, что роут динамический
+export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
     const news = await prisma.news.findMany({
